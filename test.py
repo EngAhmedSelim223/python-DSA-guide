5be32a4ffe164ce476c87fcdb4d38c078821df28
Ahmed Selim <ahmedselimelsayed@gmail.com>
2025-07-13 12:02:43 +0300

    BigO complete.

---
+++ test.py
@@ -0,0 +1,7 @@
+num1 = 13
+num2 = num1
+print("Before num2 value is updated..")
+print(f"num1 = {num1}")
+print(f"num2 = {num2}")
+print(f"num1 points to: {id(num1)}")
+print(f"num2 points to: {id(num2)}")
