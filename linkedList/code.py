9d00fbe9673c012e5ace3c658d21971c1549adfe
Ahmed Selim <ahmedselimelsayed@gmail.com>
2025-07-16 12:14:54 +0300

    linked list implementation

--- linkedList/code.py
+++ linkedList/code.py
@@ -30,7 +30,7 @@ class LinkedList:
             self.length += 1
 
 
-    # NOTE:(Function 2)  Pop Last element from the linked list
+    # NOTE:(Function 3)  Pop Last element from the linked list
     def pop(self):
         if self.length == 0:
             return None
@@ -49,7 +49,7 @@ class LinkedList:
                 self.tail = None
             return temp.value
 
-    # NOTE:(Function 3)  add item at the beginning of linked list
+    # NOTE:(Function 4)  add item at the beginning of linked list
     def prepend(self, value):
         node_to_append = Node(value)
         if self.length == 0:
@@ -60,7 +60,7 @@ class LinkedList:
             self.head = node_to_append
         self.length += 1
 
-    # NOTE:(Function 4)  Pop the first item from linked list
+    # NOTE:(Function 5)  Pop the first item from linked list
     def pop_first(self):
         if self.length == 0:
             return None
@@ -74,7 +74,7 @@ class LinkedList:
                 self.tail = None
             return poped_node
 
-    # NOTE:(Function 5) Get element at index
+    # NOTE:(Function 6) Get element at index
     def get(self, index):
         if index >= self.length or index < 0:
             return None
@@ -83,15 +83,60 @@ class LinkedList:
             for _ in range(index):
                 temp = temp.next
             return temp
-    
-    # NOTE: (Function 6) Set element in index
+
+    # NOTE: (Function 7) Set element in index
     def set_value(self, index, value):
         temp = self.get(index)
         if temp:
             temp.value = value
             return True
         return False
-    # NOTE:(Last Function) Print Linked List Elements
+
+    # NOTE: (Function 8) insert new Node
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+        elif index == 0:
+            return self.prepend(value)
+        elif index == self.length:
+            return self.append(value)
+        else:
+            new_node = Node(value)
+            temp = self.get(index - 1)
+            new_node.next = temp.next
+            temp.next = new_node
+            self.length += 1
+            return True
+
+    # NOTE: (Function 9) Remove Node in an index from the linked List
+    def remove(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        elif index == 0:
+            return self.pop_first()
+        elif index == self.length - 1:
+            return self.pop()
+        else:
+            prev = self.get(index - 1)
+            temp = prev.next
+            prev.next = temp.next
+            temp.next = None
+            self.length -= 1
+            return temp
+    
+    # NOTE: (Function 10) Reverse linked list
+    def reverse(self):
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+        after = temp.next
+        before = None
+        for _ in range(self.length):
+            after = temp.next
+            temp.next = before
+            before = temp
+            temp = after
+    # NOTE:(Function 2) Print Linked List Elements
     def print_list(self):
         temp = self.head
         while temp is not None:
